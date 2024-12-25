8ffdf49759da6552d6d37c54d5fe983818c36790
Mahmoud Mostafa <86695350+mahmoudjhonny@users.noreply.github.com>
2024-12-25 19:54:43 +0200

    Add Navbar content

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,53 @@
+import { useState } from "react";
+
 const NavBar = () => {
-  return <div>NAVBAR</div>;
+  const [open, setOpen] = useState(false);
+  return (
+    <div className="w-full h-16 md:h-20 flex items-center justify-between">
+      {/* LOGO */}
+      <div className="flex items-center gap-4 text-2xl font-bold">
+        <img className="w-8" src="/logo.png" alt="" />
+        <span>Lama Logo</span>
+      </div>
+      {/* MOBILE MENU */}
+      <div className="md:hidden">
+        <div
+          className="cursor-pointer text-4xl"
+          onClick={() => setOpen((prev) => !prev)}
+        >
+          {open ? "X" : "Y"}
+        </div>
+        {/* Mobile Link List */}
+        <div
+          className={`w-full h-screen flex flex-col items-center justify-center transition-all gap-8 font-medium text-lg ease-in-out absolute top-16 ${
+            open ? "-right-0" : "-right-[100%]"
+          }`}
+        >
+          <a href="">Home</a>
+          <a href="">Trending</a>
+          <a href="">Most Popular</a>
+          <a href="">About</a>
+          <a href="">
+            <button className="py-2 px-4 bg-blue-800 rounded-3xl text-white">
+              Login 👋
+            </button>
+          </a>
+        </div>
+      </div>
+      {/* DESKTOP MENU */}
+      <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
+        <a href="">Home</a>
+        <a href="">Trending</a>
+        <a href="">Most Popular</a>
+        <a href="">About</a>
+        <a href="">
+          <button className="py-2 px-4 bg-blue-800 rounded-3xl text-white">
+            Login 👋
+          </button>
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
